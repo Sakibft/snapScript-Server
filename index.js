b9476cecf751428351a39fbff0123510a78858b2
Sakib <ssar96540@gmail.com>
2024-05-13 00:14:45 +0600

    Added 2 opertaion >> Post=> comment  , get comment with => specific id

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ const client = new MongoClient(uri, {
 async function run() {
   const blogsCollection = client.db("b9a11").collection("allBlogs");
   const wishCollection = client.db("b9a11").collection("wish");
+  const commentCollection = client.db("b9a11").collection("comment");
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
@@ -98,18 +99,43 @@ async function run() {
       console.log(wish,'jajaj');
     })
   //  get wish
- 
   app.get('/wish/:email', async(req,res)=>{
     const email = req.params.email;
-    
     const query = {userEmail : email}
-    // console.log(user);
    const wish =  wishCollection.find(query)
    const result = await wish.toArray()
    console.log(result);
    res.send(result)
   })
-  
+// delete from wishlist
+  app.delete('/delete/:id',async(req,res)=>{
+    const id = req.params.id;
+    const query = {_id : new ObjectId(id)}
+    const result = await wishCollection.deleteOne(query)
+    res.send(result)
+    console.log(id);
+    console.log(result);
+  })
+// comment in the details page 
+app.post('/comment',async(req,res)=> {
+  const comment = req.body;
+  const result = await commentCollection.insertOne(comment)
+  res.send(result)
+  console.log(comment);
+})
+// get comment 
+app.get('/comment/:id', async(req,res)=>{
+   const id = req.params.id;
+   const query = {blogid:id}
+   const comment =  commentCollection.find(query)
+   const result = await comment.toArray();
+   res.send(result)
+   console.log(result);
+   console.log(result);
+   console.log(id,'client');
+ 
+})
+ 
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
